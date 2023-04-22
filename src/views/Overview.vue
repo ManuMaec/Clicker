b6a9f473f5db79bc5f4abd918e69044d584ac2b7
ManuMaec <manu.escal3000@gmail.com>
2023-04-22 03:58:39 +0200

    Haciendo mi primer cambio
    He hecho mis primeras funcionalidades con vue, ahora toca hacer el juego

---
+++ src/views/Overview.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import {ref} from "vue";
+
+let msg = ref("Hola con reactividad atentamente dios")
+let entre = ref("Quita eso de mi cara")
+let leave = ref ("Gracias, basura... Hola con reactividad atentamente dios")
+
+</script>
+<template>
+    <h1 @click="() => msg= msg+'!'" @mouseenter=" () => msg=entre" @mouseleave=" () => msg = leave">Hola mundo</h1>
+    <h3>{{msg}}</h3>
+</template>
