9771515845b9702e44b6dac57a8d4028cefffb2f
Wladimir <wladimirss14@gmail.com>
2023-04-30 23:21:35 -0400

    Editando la tarjeta egresos

--- src/components/TarjetaEgresos.vue
+++ src/components/TarjetaEgresos.vue
@@ -4,16 +4,17 @@ import { ref, toRefs, onMounted } from "vue";
 const props = defineProps({
   nombre: String,
   costo: Number,
-  ingresos: Number,
-  numNegocios: Number,
+  ingresosPersonas: Number,
+  egresos: Number,
+  numServicios: Number,
   nivel: Number,
   estado: Boolean,
   cambiarEstado: Function,
-  sumarNegocio: Function,
+  sumarServicio: Function,
   subirNivel: Function
 });
 
-const { nombre, costo, ingresos, numNegocios, nivel, estado } = toRefs(props);
+const { nombre, costo, ingresosPersonas, egresos, numServicios, nivel, estado } = toRefs(props);
 
 let isHover = ref(false);
 
@@ -33,22 +34,24 @@ const sali = ()=>{
   >
     <div class="profit-card-locke" v-if="!estado">
       <h1 class="profit-card--title">{{ nombre }}</h1>
-      <p class="profit-card--atributes">Ingresos: {{ ingresos }}</p>
+      <p class="profit-card--atributes">Ingresos Personas: {{ ingresosPersonas }}</p>
+      <p class="profit-card--atributes">Egresos: {{ egresos }}</p>
       <p class="profit-card--atributes">Costo: {{ costo }}</p>
       <button class="profit-card--button" @click="props.cambiarEstado()">desbloquear</button>
-    </div>
-    <div
+      </div>
+      <div
       class="profit-card-unlocke"
       v-else
       :style=" isHover? 'opacity: 1' : 'opacity: 0' "
-    >
+      >
       <h1 class="profit-card--title">{{ nombre }}</h1>
-      <p class="profit-card--atributes">Ingresos: {{ ingresos }}</p>
+      <p class="profit-card--atributes">Ingresos: {{ ingresosPersonas }}</p>
+      <p class="profit-card--atributes">Egresos: {{ egresos }}</p>
       <p class="profit-card--atributes">Nivel: {{ nivel }}</p>
-      <p class="profit-card--atributes">Locales: adquiridos: {{ numNegocios }}</p>
+      <p class="profit-card--atributes">Locales: adquiridos: {{ numServicios }}</p>
       <div class="profit-card__actions">
         <button class="profit-card--button" @click="props.subirNivel()">Subir Nivel</button>
-        <button class="profit-card--button" @click="props.sumarNegocio()">Comprar local</button>
+        <button class="profit-card--button" @click="props.sumarServicio()">Comprar local</button>
       </div>
     </div>
   </div>
